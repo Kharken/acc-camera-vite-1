05de603d64ef41aa60373bcef8796d652cf10421
Yuriy Andreev <andreyev.yury@gmail.com>
2025-03-09 13:50:03 +0300

    ReactFocusLock работает

--- src/pages/catalog-page/components/catalog/components/catalog-call-item/catalog-call-item.tsx
+++ src/pages/catalog-page/components/catalog/components/catalog-call-item/catalog-call-item.tsx
@@ -1,4 +1,4 @@
-
+import ReactFocusLock from 'react-focus-lock';
 import {CatalogCallItemProps} from '../product-card/types/types.ts';
 import {useEffect, useRef} from 'react';
 import {useAppSelector} from '../../../../../../app/hooks/hooks.ts';
@@ -29,86 +29,92 @@ const CatalogCallItem = ({handleModalCloseClick, isModalOpen, activeCard}: Catal
   }, [handleModalCloseClick, isModalOpen, focusRef]);
 
   return (
-    <div className="modal is-active">
-      <div className="modal__wrapper">
-        <div className="modal__overlay" onClick={handleModalCloseClick}>
-
-        </div>
-        <div className="modal__content">
-          <p className="title title--h4">Свяжитесь со мной</p>
-          <div className="basket-item basket-item--short">
-            <div className="basket-item__img">
-              <picture>
-                <source type="image/webp"
-                  srcSet={currentActiveCard && `./${currentActiveCard.previewImgWebp}, ../${currentActiveCard.previewImgWebp2x}`}
-                />
-                <img src={currentActiveCard && currentActiveCard.previewImg}
-                  srcSet={currentActiveCard && currentActiveCard.previewImg2x}
-                  width="140"
-                  height="120"
-                  alt={currentActiveCard && currentActiveCard.name}
-                />
-              </picture>
-            </div>
-            <div className="basket-item__description">
-              <p className="basket-item__title">{currentActiveCard && currentActiveCard.name}</p>
-              <ul className="basket-item__list">
-                <li className="basket-item__list-item"><span className="basket-item__article">Артикул:</span>
-                  <span className="basket-item__number">{currentActiveCard && currentActiveCard.vendorCode}2</span>
-                </li>
-                <li className="basket-item__list-item">{currentActiveCard && currentActiveCard.type}</li>
-                <li className="basket-item__list-item">{currentActiveCard && currentActiveCard.level} уровень</li>
-              </ul>
-              <p className="basket-item__price"><span className="visually-hidden">Цена:</span>{currentActiveCard && currentActiveCard.price} ₽</p>
-            </div>
-          </div>
-          <div className="custom-input form-review__item">
-            <label>
-              <span className="custom-input__label">Телефон
-                <svg width="9"
-                  height="9"
-                  aria-hidden="true"
-                >
-                  <use xlinkHref="#icon-snowflake"></use>
-                </svg>
-              </span>
-              <input type="tel"
-                name="user-tel"
-                placeholder="Введите ваш номер"
-                required
-                ref={focusRef}
-              />
-            </label>
-            <p className="custom-input__error">Нужно указать номер</p>
-          </div>
-          <div className="modal__buttons">
-            <button className="btn btn--purple modal__btn modal__btn--fit-width"
-              type="button"
-            >
-              <svg width="24"
-                height="16"
-                aria-hidden="true"
-              >
-                <use xlinkHref="#icon-add-basket"></use>
-              </svg>
-              Заказать
-            </button>
-          </div>
-          <button className="cross-btn"
-            type="button"
-            aria-label="Закрыть попап"
+    <ReactFocusLock>
+      <div className="modal is-active">
+        <div className="modal__wrapper">
+          <div className="modal__overlay"
             onClick={handleModalCloseClick}
           >
-            <svg width="10"
-              height="10"
-              aria-hidden="true"
+
+          </div>
+          <div className="modal__content">
+            <p className="title title--h4">Свяжитесь со мной</p>
+            <div className="basket-item basket-item--short">
+              <div className="basket-item__img">
+                <picture>
+                  <source type="image/webp"
+                    srcSet={currentActiveCard && `./${currentActiveCard.previewImgWebp}, ../${currentActiveCard.previewImgWebp2x}`}
+                  />
+                  <img src={currentActiveCard && currentActiveCard.previewImg}
+                    srcSet={currentActiveCard && currentActiveCard.previewImg2x}
+                    width="140"
+                    height="120"
+                    alt={currentActiveCard && currentActiveCard.name}
+                  />
+                </picture>
+              </div>
+              <div className="basket-item__description">
+                <p className="basket-item__title">{currentActiveCard && currentActiveCard.name}</p>
+                <ul className="basket-item__list">
+                  <li className="basket-item__list-item"><span className="basket-item__article">Артикул:</span>
+                    <span className="basket-item__number">{currentActiveCard && currentActiveCard.vendorCode}2</span>
+                  </li>
+                  <li className="basket-item__list-item">{currentActiveCard && currentActiveCard.type}</li>
+                  <li className="basket-item__list-item">{currentActiveCard && currentActiveCard.level} уровень</li>
+                </ul>
+                <p className="basket-item__price">
+                  <span className="visually-hidden">Цена:</span>{currentActiveCard && currentActiveCard.price} ₽
+                </p>
+              </div>
+            </div>
+            <div className="custom-input form-review__item">
+              <label>
+                <span className="custom-input__label">Телефон
+                  <svg width="9"
+                    height="9"
+                    aria-hidden="true"
+                  >
+                    <use xlinkHref="#icon-snowflake"></use>
+                  </svg>
+                </span>
+                <input type="tel"
+                  name="user-tel"
+                  placeholder="Введите ваш номер"
+                  required
+                  ref={focusRef}
+                />
+              </label>
+              <p className="custom-input__error">Нужно указать номер</p>
+            </div>
+            <div className="modal__buttons">
+              <button className="btn btn--purple modal__btn modal__btn--fit-width"
+                type="button"
+              >
+                <svg width="24"
+                  height="16"
+                  aria-hidden="true"
+                >
+                  <use xlinkHref="#icon-add-basket"></use>
+                </svg>
+                Заказать
+              </button>
+            </div>
+            <button className="cross-btn"
+              type="button"
+              aria-label="Закрыть попап"
+              onClick={handleModalCloseClick}
             >
-              <use xlinkHref="#icon-close"></use>
-            </svg>
-          </button>
+              <svg width="10"
+                height="10"
+                aria-hidden="true"
+              >
+                <use xlinkHref="#icon-close"></use>
+              </svg>
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </ReactFocusLock>
   );
 };
 
