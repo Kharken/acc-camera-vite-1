a39f404504bd386ce28d7428ce756f71dcb33624
Yuriy Andreev <andreyev.yury@gmail.com>
2025-03-12 22:08:25 +0300

    get-enum-text-keys passed

---
+++ src/utils/get-enum-text-keys.test.ts
@@ -0,0 +1,19 @@
+import {describe, expect} from 'vitest';
+import {getEnumTextKeys} from './get-enum-text-keys.ts';
+import {enumTextKeysResultOne, enumTextKeysResultTwo, enumTextKeysResultThree, enumTextKeysTestThree, enumTextKeysTestOne, enumTextKeysTestTwo} from '../mocks/get-enum-text-keys-test-mocks.ts';
+
+describe('function: getEnumTextKeys', () => {
+  it('should return an array of \' Not-a-number\' keys from every enum', () => {
+    const enumTextKeysTestResultOne = JSON.stringify(getEnumTextKeys(enumTextKeysTestOne));
+    const enumTextKeysTestResultTwo = JSON.stringify(getEnumTextKeys(enumTextKeysTestTwo));
+    const enumTextKeysTestResultThree = JSON.stringify(getEnumTextKeys(enumTextKeysTestThree));
+
+    const correctEnumTextKeysTestResultOne = JSON.stringify(enumTextKeysResultOne);
+    const correctEnumTextKeysTestResultTwo = JSON.stringify(enumTextKeysResultTwo);
+    const correctEnumTextKeysTestResultThree = JSON.stringify(enumTextKeysResultThree);
+
+    expect(correctEnumTextKeysTestResultOne).toBe(enumTextKeysTestResultOne);
+    expect(correctEnumTextKeysTestResultTwo).toBe(enumTextKeysTestResultTwo);
+    expect(correctEnumTextKeysTestResultThree).toBe(enumTextKeysTestResultThree);
+  });
+});
--- src/utils/get-sorted-list.test.ts
+++ src/utils/get-sorted-list.test.ts
@@ -4,7 +4,7 @@ import {sortedByDateList, sortedByUserList, sortedByAgeList, mockTestSortedList}
 import dayjs from 'dayjs';
 
 describe('Fucnction: getSortedList', () => {
-  it('should return sorted by prop \'date\' array', () => {
+  it('should return sorted by growth every prop array', () => {
     const sortedByDateTestListResult = JSON.stringify(getSortedList(mockTestSortedList, (a, b) => dayjs(a.date).unix() - dayjs(b.date).unix()));
     const sortedByUserTestListResult = JSON.stringify(getSortedList(mockTestSortedList, (a, b) => a.user.localeCompare(b.user)));
     const sortedByAgeTestListResult = JSON.stringify(getSortedList(mockTestSortedList, (a, b) => a.age - b.age));
