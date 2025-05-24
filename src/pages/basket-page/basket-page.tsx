c30cc93b1fed4eef8e4f970da1d0e468e7ea8f58
Yuriy Andreev <165748827+Kharken@users.noreply.github.com>
2025-05-24 17:40:16 +0000

    some features

--- src/pages/basket-page/basket-page.tsx
+++ src/pages/basket-page/basket-page.tsx
@@ -1,33 +1,35 @@
+import { useEffect, useState } from 'react';
+import { calculateDiscount } from '../../utils/functions/calculate-discount';
 
-import {useEffect, useState} from 'react';
-import {calculateDiscount} from '../../utils/functions/calculate-discount';
-
-import {useAppDispatch, useAppSelector} from '../../app/hooks/hooks.ts';
-import {getBasketCameraList} from '../../store/slice/basket-slice/service/basket-selectors.ts';
+import { useAppDispatch, useAppSelector } from '../../app/hooks/hooks.ts';
+import { getBasketCameraList } from '../../store/slice/basket-slice/service/basket-selectors.ts';
 import BasketItem from './components/basket-item';
-import {getUniqueList} from '../../utils/functions/get-unique-list.ts';
-import {CAMERA_CART_LOCALSTORAGE_KEY} from '../../utils/const/const.ts';
-import {getFromLocalStorage} from '../catalog-page/utils';
-import {Camera} from '../../store/slice/camera-slice/types/types.ts';
-import {setBasketData} from '../../store/slice/basket-slice/service/basket-slice.ts';
+import { getUniqueList } from '../../utils/functions/get-unique-list.ts';
+import { CAMERA_CART_LOCALSTORAGE_KEY } from '../../utils/const/const.ts';
+import { getFromLocalStorage } from '../catalog-page/utils';
+import { Camera } from '../../store/slice/camera-slice/types/types.ts';
+import { setBasketData } from '../../store/slice/basket-slice/service/basket-slice.ts';
 import RemoveModalItem from './components/remove-modal-item';
-import {useMouseModal} from '../../utils/hooks/useMouseModal.ts';
+import { useMouseModal } from '../../utils/hooks/useMouseModal.ts';
 
 const BasketPage = () => {
   const dispatch = useAppDispatch();
   const basketStorageData = useAppSelector(getBasketCameraList);
-  const basketTotalPrice = basketStorageData.reduce((acc, item) => acc + item.price, 0) || 0;
+  const basketTotalPrice =
+    basketStorageData.reduce((acc, item) => acc + item.price, 0) || 0;
   const totalItems = basketStorageData.length;
   const discount = calculateDiscount({
     totalItems,
     totalPrice: basketTotalPrice,
-    isPromo: false
+    isPromo: false,
   });
 
   useEffect(() => {
     const handleStorageUpdate = (e: StorageEvent) => {
       if (e.key === CAMERA_CART_LOCALSTORAGE_KEY) {
-        const newData = getFromLocalStorage<Camera>(CAMERA_CART_LOCALSTORAGE_KEY);
+        const newData = getFromLocalStorage<Camera>(
+          CAMERA_CART_LOCALSTORAGE_KEY,
+        );
         dispatch(setBasketData(newData));
       }
     };
@@ -35,7 +37,8 @@ const BasketPage = () => {
     window.addEventListener('storage', handleStorageUpdate);
     return () => window.removeEventListener('storage', handleStorageUpdate);
   }, [dispatch]);
-  const { activeModal, handleModalCloseClick, handleModalOpenClick} = useMouseModal();
+  const { activeModal, handleModalCloseClick, handleModalOpenClick } =
+    useMouseModal();
 
   const basketData = getUniqueList(basketStorageData, 'id');
 
@@ -45,41 +48,32 @@ const BasketPage = () => {
     handleModalCloseClick();
   };
 
-
   return (
     <>
-      <div className="page-content"
-        data-testid="basket-page-content"
-      >
+      <div className="page-content" data-testid="basket-page-content">
         <div className="breadcrumbs">
           <div className="container">
             <ul className="breadcrumbs__list">
               <li className="breadcrumbs__item">
-                <a className="breadcrumbs__link"
-                  href="index.html"
-                >Главная
-                  <svg width="5"
-                    height="8"
-                    aria-hidden="true"
-                  >
+                <a className="breadcrumbs__link" href="index.html">
+                  Главная
+                  <svg width="5" height="8" aria-hidden="true">
                     <use xlinkHref="#icon-arrow-mini"></use>
                   </svg>
                 </a>
               </li>
               <li className="breadcrumbs__item">
-                <a className="breadcrumbs__link"
-                  href="catalog.html"
-                >Каталог
-                  <svg width="5"
-                    height="8"
-                    aria-hidden="true"
-                  >
+                <a className="breadcrumbs__link" href="catalog.html">
+                  Каталог
+                  <svg width="5" height="8" aria-hidden="true">
                     <use xlinkHref="#icon-arrow-mini"></use>
                   </svg>
                 </a>
               </li>
               <li className="breadcrumbs__item">
-                <span className="breadcrumbs__link breadcrumbs__link--active">Корзина</span>
+                <span className="breadcrumbs__link breadcrumbs__link--active">
+                  Корзина
+                </span>
               </li>
             </ul>
           </div>
@@ -88,45 +82,58 @@ const BasketPage = () => {
           <div className="container">
             <h1 className="title title--h2">Корзина</h1>
             <ul className="basket__list">
-              {basketData.map((item) => (<BasketItem basketStorageData={basketStorageData} props={item} key={item.id} handleModalOpenClick={handleModalOpenClick} />))}
+              {basketData.map((item) => (
+                <BasketItem
+                  basketStorageData={basketStorageData}
+                  props={item}
+                  key={item.id}
+                  handleModalOpenClick={handleModalOpenClick}
+                />
+              ))}
             </ul>
             <div className="basket__summary">
-              <div className="basket__promo">
-              </div>
+              <div className="basket__promo"></div>
               <div className="basket__summary-order">
                 <p className="basket__summary-item">
-                  <span className="basket__summary-text">Всего:</span><span className="basket__summary-value">{basketTotalPrice} ₽</span>
+                  <span className="basket__summary-text">Всего:</span>
+                  <span className="basket__summary-value">
+                    {basketTotalPrice} ₽
+                  </span>
                 </p>
                 <p className="basket__summary-item">
                   <span className="basket__summary-text">Скидка:</span>
-                  <span className={`basket__summary-value ${discount > 0 ? 'basket__summary-value--bonus' : ''}`}>
+                  <span
+                    className={`basket__summary-value ${discount > 0 ? 'basket__summary-value--bonus' : ''}`}
+                  >
                     {discount.toFixed(0)} ₽
                   </span>
                 </p>
                 <p className="basket__summary-item">
-                  <span className="basket__summary-text basket__summary-text--total">К оплате:</span>
+                  <span className="basket__summary-text basket__summary-text--total">
+                    К оплате:
+                  </span>
                   <span className="basket__summary-value basket__summary-value--total">
                     {(basketTotalPrice - discount).toFixed(0)} ₽
                   </span>
                 </p>
-                </p>
-                <button className="btn btn--purple"
-                  type="submit"
-                >Оформить заказ
+
+                <button className="btn btn--purple" type="submit">
+                  Оформить заказ
                 </button>
               </div>
             </div>
           </div>
         </section>
       </div>
-      {activeModal.isModalOpen &&
+      {activeModal.isModalOpen && (
         <RemoveModalItem
           handleModalCloseClick={handleModalCloseClick}
           isModalOpen={activeModal.isModalOpen}
           activeCard={activeModal.activeCard}
           basketData={basketData}
           clickButtonRemoveItemHandler={clickButtonRemoveItemHandler}
-        />}
+        />
+      )}
     </>
   );
 };
